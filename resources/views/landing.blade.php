b8381d0fab2dcfe54539ea3740e417f77923e0c1
Njazi Shehu <shehu.nj@gmail.com>
2020-05-18 01:15:58 +0200

    Added landing page changes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,12 +6,12 @@
         <section class="section-header pb-7 pb-lg-11 bg-soft">
             <div class="container">
                 <div class="row justify-content-between align-items-center mt-5 mt-md-0">
-                    <div class="col-12 col-md-6 order-2 order-lg-1">
-                        <img src="{{asset('storage/images/hero-illustration.svg')}}" alt="">
+                    <div class="col-12 col-md-6 order-2 order-lg-1 mb-5 mb-md-0">
+                        <img class="w-100"src="{{asset('storage/images/grow-visual.svg')}}" alt="">
                     </div>
                     <div class="col-12 col-md-5 order-1 order-lg-2 mb-5 mb-md-0">
-                          <h1 class="display-2 mb-3">Tools to grow your traffic</h1>
-                          <p class="lead">Impact helps you learn why your competitors rank so high and what you need to do to outrank them.</p>
+                          <h1 class="display-2 mb-3">Grow your traffic</h1>
+                          <p class="lead font-weight-normal">Enrich your feeds, add links to drive traffic from Instagram</p>
                           <div class="mt-4">
                             <button class="btn btn-primary w-100" type="submit">Create your account</button>
 
@@ -26,27 +26,28 @@
             <div class="container">
                 <div class="row justify-content-center mb-5 mb-md-7">
                     <div class="col-12 col-md-8 text-center">
-                        <h2 class="h1 font-weight-bolder mb-4">Meet our intuitive platform</h2>
-                        <p class="lead">Self-Service Analytics or ad hoc reporting gives users the ability to develop rapid reports, empowering users to analyze their data.</p>
+                        <h2 class="h1 font-weight-bolder mb-4">Meet Our Product</h2>
+                        <p class="lead font-weight-normal">Feeds lets you add links on your posts and drive traffic to your website direct from your Instagram Bio.</p>
                     </div>
                 </div>
                 <div class="row row-grid align-items-center mb-5 mb-md-7">
                     <div class="col-12 col-md-5">
-                        <h2 class="font-weight-bolder mb-4">Site Audit</h2>
-                        <p class="lead">Site Audit crawls all the pages it finds on your website – then provides an overall SEO health score, visualises key data in charts, flags all possible SEO issues and provides recommendations on how to fix them.</p>
-                        <p class="lead">Have a huge website? Not an issue.</p>
-                        <a href="./about.html" class="btn btn-primary mt-3 animate-up-2">
+                        <h2 class="font-weight-bolder mb-4">How it works</h2>
+                        <p class="lead font-weight-normal">Add a link of your articles and webpages in photos and videos you post on Instagram, and drive traffic from the Link in Bio. All posts that contain links will appear in the link that you post in your profile bio. This will help you enrich your posts and also increase the traffic to your website from Instagram.</p>
+                        <a href="" class="btn btn-primary mt-3 animate-up-2">
                             Learn More
                             <span class="icon icon-xs ml-2">
                                 <i class="fas fa-external-link-alt"></i>
                             </span>
                         </a>
                     </div>
-                    <div class="col-12 col-md-6 ml-md-auto">
-                        <img src="{{asset('storage/images/feature-illustration.svg')}}" alt="">
+                    <div class="col-12 col-md-6 ml-md-auto mt-5 mt-md-0">
+                        <img class="w-100"src="{{asset('storage/images/mobile-feed.svg')}}" alt="">
                     </div>
                 </div>
-                <div class="row row-grid align-items-center mb-5 mb-md-7">
+
+
+                <!-- <div class="row row-grid align-items-center mb-5 mb-md-7">
                     <div class="col-12 col-md-5 order-md-2">
                         <h2 class="font-weight-bolder mb-4">Rank Tracker</h2>
                         <p class="lead">We track your desktop and mobile keyword rankings from any location and plot your full ranking history on a handy graph.</p>
@@ -61,7 +62,7 @@
                     <div class="col-12 col-md-6 mr-lg-auto">
                         <img src="{{asset('storage/images/feature-illustration-2.svg')}}" alt="">
                     </div>
-                </div>
+                </div> -->
 
                 <!-- ToDo When we get real clients show numbers -->
 
@@ -92,17 +93,16 @@
                     </div> -->
 
 
-
                 </div>
             </div>
         </section>
 
 
         
-        <section class="section section-lg pb-5 bg-soft">
+        <section class="section section-lg pt-5 pb-5 bg-soft">
             <div class="container">
                 <div class="row"> 
-                    <div class="col-12 text-center mb-5">
+                    <div class="col-12 text-center">
                         <h2 class="mb-4">Faster growth starts with Impact</h2>
                         <p class="lead mb-5">Join over <span class="font-weight-bolder">300,000+</span> users</p>
                     </div>
@@ -115,7 +115,7 @@
 
 
         <footer class="footer section pt-6 pt-md-8 pt-lg-10 pb-3 bg-primary text-white overflow-hidden">
-            <div class="pattern pattern-soft top"></div>
+            <!-- <div class="pattern pattern-soft top"></div> -->
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4 mb-4 mb-lg-0">
